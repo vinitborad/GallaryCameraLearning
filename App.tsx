c8fff4ab71b7255d8a6127184b5028cf7291a64b
vinitborad <vinitborad12@gmail.com>
2023-12-13 12:50:25 +0530

    Committing whole project

--- App.tsx
+++ App.tsx
@@ -1,118 +1,147 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { CameraRoll, PhotoIdentifier } from '@react-native-camera-roll/camera-roll';
+import React, { useEffect } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  Dimensions,
+  FlatList,
+  Image,
+  PermissionsAndroid,
+  Platform,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import Torch from 'react-native-torch';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const [photos, setPhotos] = React.useState<PhotoIdentifier[]>();
+  const [torchStatus, setTorchStatus] = React.useState<boolean>(false);
+
+  async function hasAndroidPermission() {
+    const getCheckPermissionPromise = () => {
+      if (Platform.Version >= 33) {
+        return Promise.all([
+          PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES),
+          PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO),
+        ]).then(
+          ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
+            hasReadMediaImagesPermission && hasReadMediaVideoPermission,
+        );
+      } else {
+        return PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
+      }
+    };
+
+    const hasPermission = await getCheckPermissionPromise();
+    if (hasPermission) {
+      return true;
+    }
+    const getRequestPermissionPromise = () => {
+      if (Platform.Version >= 33) {
+        return PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+          PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+        ]).then(
+          (statuses) =>
+            statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+            statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
+            PermissionsAndroid.RESULTS.GRANTED,
+        );
+      } else {
+        return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE).then((status) => status === PermissionsAndroid.RESULTS.GRANTED);
+      }
+    };
+
+    return await getRequestPermissionPromise();
+  }
+
+  useEffect(() => {
+    if (Platform.OS === "android" && !(hasAndroidPermission())) {
+      console.log("GOT PERMISSION");
+      return;
+    }
+  }, []);
+
+  const getAllPhotos = () => {
+    CameraRoll.getPhotos({
+      first: 200,
+      assetType: 'Photos',
+    })
+      .then(r => {
+        setPhotos(r.edges);
+      })
+      .catch((err) => {
+        //Error Loading Images
+      });
+  };
+
+  const toggleFlashlight = async () => {
+    try {
+      // const isTorchOn = await Torch.isTorchOn();
+      await Torch.switchState(!torchStatus); // Toggle based on current state
+      setTorchStatus(!torchStatus);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+
+    <View style={{ flex: 1 }}>
+
+      <View style={{ width: '100%', alignItems: 'center' }}>
+        <FlatList
+          data={photos}
+          numColumns={2}
+          renderItem={({ item, index }) => {
+            return (
+              <View style={{
+                width: Dimensions.get("window").width / 2 - 20,
+                height: 200,
+                borderRadius: 10,
+                backgroundColor: "#F7BBBB",
+                margin: 10,
+              }}>
+                <Image
+                  source={{ uri: item.node.image.uri }}
+                  style={{
+                    width: "95%",
+                    height: "95%",
+                    alignSelf: "center",
+                  }}
+                />
+              </View>
+            );
+          }}
+        />
+      </View>
+
+      <TouchableOpacity
+        style={{
+          width: "90%",
+          height: 50,
+          backgroundColor: "#4054EB",
+          justifyContent: "center",
+          alignItems: "center",
+          alignSelf: "center",
+          position: "absolute",
+          bottom: 20,
+          borderRadius: 100,
+        }}
+        onPress={() => {
+          console.log("PRESSED");
+          toggleFlashlight();
+          getAllPhotos();
+        }}
+      >
+        <Text style={{ color: "#fff" }}>Sync Photos & Toggle Torch</Text>
+      </TouchableOpacity>
+
+    </View>
+    // </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
